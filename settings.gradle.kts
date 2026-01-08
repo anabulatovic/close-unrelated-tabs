c38d5967c6401dc07d4f83e65bc0756c5c544e3a
ana-bulatovic <abulatovic5@gmail.com>
2026-01-08 17:42:23 +0100

    setup

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "close-unrelated-tabs"
---
+++ src/main/java/com/anabulatovic/closeunrelatedtabs/ToolWindowFactoryImpl.java
@@ -0,0 +1,20 @@
+package com.anabulatovic.closeunrelatedtabs;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class ToolWindowFactoryImpl implements ToolWindowFactory {
+
+    @Override
+    public boolean shouldBeAvailable(@NotNull Project project) {
+        return true;
+    }
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        // impl
+    }
+
+}
