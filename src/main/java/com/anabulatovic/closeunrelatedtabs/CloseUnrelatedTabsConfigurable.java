f6d5dff460adb767ee47e7172753db4ae3503bc0
ana-bulatovic <abulatovic5@gmail.com>
2026-01-09 10:44:01 +0100

    assigned all swing components

--- src/main/java/com/anabulatovic/closeunrelatedtabs/CloseUnrelatedTabsConfigurable.java
+++ src/main/java/com/anabulatovic/closeunrelatedtabs/CloseUnrelatedTabsConfigurable.java
@@ -2,6 +2,8 @@ package com.anabulatovic.closeunrelatedtabs;
 
 import com.intellij.openapi.options.Configurable;
 import com.intellij.ui.JBIntSpinner;
+import com.intellij.ui.components.JBCheckBox;
+import com.q.L.L.A.J;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.Nullable;
 
@@ -9,10 +11,10 @@ import javax.swing.*;
 
 public class CloseUnrelatedTabsConfigurable implements Configurable {
 
-    private JCheckBox showConfirmationDialogCheckBox;
-    private JCheckBox keepModifiedTabsCheckBox;
-    private JCheckBox keepCorrespondingTestFilesCheckBox;
-    private JCheckBox keepRecentlyEditedTabsCheckBox;
+    private JBCheckBox showConfirmationDialogCheckBox;
+    private JBCheckBox keepModifiedTabsCheckBox;
+    private JBCheckBox keepCorrespondingTestFilesCheckBox;
+    private JBCheckBox keepRecentlyEditedTabsCheckBox;
     private JBIntSpinner minimumTabsToKeepSpinner;
     private JBIntSpinner referenceDepthSpinner;
     private JBIntSpinner recentlyEditedMinutesSpinner;
@@ -26,6 +28,32 @@ public class CloseUnrelatedTabsConfigurable implements Configurable {
     @Override
     public @Nullable JComponent createComponent() {
         CloseUnrelatedTabsSettings settings = CloseUnrelatedTabsSettings.getInstance();
+
+        showConfirmationDialogCheckBox = new JBCheckBox(
+                MessageBundle.message("settings.show.confirmation.dialog"),
+                settings.isShowConfirmationDialog()
+        );
+
+        keepModifiedTabsCheckBox = new JBCheckBox(
+                MessageBundle.message("settings.keep.modified.tabs"),
+                settings.isKeepModifiedTabs()
+        );
+
+        keepCorrespondingTestFilesCheckBox = new JBCheckBox(
+                MessageBundle.message("settings.keep.test.files"),
+                settings.isKeepCorrespondingTestFiles()
+        );
+
+        keepRecentlyEditedTabsCheckBox = new JBCheckBox(
+                MessageBundle.message("settings.show.confirmation.dialog"),
+                settings.isKeepRecentlyEditedTabs()
+        );
+
+        recentlyEditedMinutesSpinner = new JBIntSpinner(settings.getRecentlyEditedMinutes(), 1, 120);
+
+        minimumTabsToKeepSpinner = new JBIntSpinner(settings.getMinimumTabsToKeepOpen(), 0, 100);
+        referenceDepthSpinner = new JBIntSpinner(settings.getReferenceDepth(), 1, 10);
+
         return new JComponent() {
         };
     }
